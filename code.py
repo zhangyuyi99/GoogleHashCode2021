6f378273a7a2a5578cfa5fb38d69cd8562309e0f
coconutgit <43692301+coconutgit@users.noreply.github.com>
2021-02-25 17:39:53 +0000

    i/o

--- code.py
+++ code.py
@@ -1,1 +1,11 @@
 
+# read the file of type file into content
+with open("a_example", 'r') as fp:
+    content = fp.read()
+
+print(type(content))
+print(content)
+
+with open("result.txt", "w") as text_file:
+    text_file.write(content)
+
